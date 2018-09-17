ebb9d9acd7992df43f0e7d0ba3cbae229b94551a
ayabongaqwabi <aya@simply.co.za>
2018-09-18 00:14:36 +0200

    Update blob config

--- src/App.js
+++ src/App.js
@@ -1,87 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './style/App.css';
-
-const blob1Html =
-`
-  <div className='blobs-container'>      
-      <div className='blob'>
-          <h1>Just flex 1</h1>
-      </div>
-      <div className='blob'>
-        <h1>Just flex 2</h1>
-      </div>
-   </div>
-`
-const blob2Html = 
-`
-<div className='blobs-stretch-container'>
-    <div className='blob-stretch'>
-        <h1>Just flex 1</h1>
-    </div>
-    <div className='blob-stretch'>
-      <h1>Just flex 2</h1>
-    </div>
-</div>
-`
-
-const blob3Html =
-`
-<div className='blobs-max-height-container'>
-    <div className='blob-max-height x1'>
-        <h1>Flex stretch 1</h1>
-    </div>
-    <div className='blob-max-height x2'>
-      <h1>Flex stretch 2</h1>
-    </div>
-    <div className='blob-max-height x3'>
-      <h1>Flex stretch 3</h1>
-    </div>
-    <div className='blob-max-height x4'>
-      <h1>Flex stretch 4</h1>
-    </div>
-    <div className='blob-max-height x5'>
-      <h1>Flex stretch 5</h1>
-    </div>
-</div>
-`
-
-const blob1Css =
-`
-  .blobs-container{
-      background: orange;
-      padding: 2vw;
-      display: flex;
-    }
-
-    .blob{
-      background:#fff;
-      display: flex;
-      border: 1px solid grey;
-      padding:15px;
-    }
-  `
-  const blob2Css =
-  `
-  .blobs-stretch-container{
-    background: orange;
-    padding: 2vw;
-    display: flex;
-    flex-direction: row;
-  }
-  .blob-stretch{
-    background:#fff;
-    display: flex;
-    border: 1px solid grey;
-    padding:15px;
-    flex:1;
-  }
-  `
-
-  const blob3Css =
-  `
-  .blobs-max-height-container{
-  `
+import { blob1Html, blob2Html, blob3Html, blob1Css, blob2Css, blob3Css } from './blobs';
 class App extends Component {
   render() {
     return (
@@ -184,15 +104,6 @@ class App extends Component {
                 </div>
                 <div className='blob-max-height x2'>
                   <h1>Flex stretch 2</h1>
-                </div>
-                <div className='blob-max-height x3'>
-                  <h1>Flex stretch 3</h1>
-                </div>
-                <div className='blob-max-height x4'>
-                  <h1>Flex stretch 4</h1>
-                </div>
-                <div className='blob-max-height x5'>
-                  <h1>Flex stretch 5</h1>
                 </div>
             </div>
             </div>
---
+++ src/blobs.js
@@ -0,0 +1,91 @@
+export const blob1Html =
+`
+  <div className='blobs-container'>      
+      <div className='blob'>
+          <h1>Just flex 1</h1>
+      </div>
+      <div className='blob'>
+        <h1>Just flex 2</h1>
+      </div>
+   </div>
+   
+`
+export const blob2Html = 
+`
+<div className='blobs-stretch-container'>
+    <div className='blob-stretch'>
+        <h1>Just flex 1</h1>
+    </div>
+    <div className='blob-stretch'>
+      <h1>Just flex 2</h1>
+    </div>
+</div>
+
+`
+
+export const blob3Html =
+`
+<div className='blobs-max-height-container'>
+    <div className='blob-max-height x1'>
+        <h1>Flex stretch 1</h1>
+    </div>
+    <div className='blob-max-height x2'>
+      <h1>Flex stretch 2</h1>
+    </div>
+</div>
+
+`
+
+export const blob1Css =
+`
+  .blobs-container{
+      background: orange;
+      padding: 2vw;
+      display: flex;
+    }
+
+    .blob{
+      background:#fff;
+      display: flex;
+      border: 1px solid grey;
+      padding:15px;
+    }
+  `
+  export const blob2Css =
+  `
+  .blobs-stretch-container{
+    background: orange;
+    padding: 2vw;
+    display: flex;
+    flex-direction: row;
+  }
+  .blob-stretch{
+    background:#fff;
+    display: flex;
+    border: 1px solid grey;
+    padding:15px;
+    flex:1;
+  }
+
+  `
+
+  export const blob3Css =
+  `
+  .blobs-max-height-container{
+    background: orange;
+    padding: 2vw;
+    display: flex;
+    flex-direction: row;
+  }
+  .blob-max-height{
+    background:#fff;
+    border: 1px solid grey;
+    padding:15px;
+    display: flex;
+    flex:1;
+  }
+  .x3{
+    flex:3;
+  }
+
+  `
